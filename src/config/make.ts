9f492a315254b86ac9589b6fb7a484b8eb7d0ed1
gutisrb <johhnylaa@gmail.com>
2025-10-08 13:51:47 +0200

    Updated make.ts

--- src/config/make.ts
+++ src/config/make.ts
@@ -1,11 +1,11 @@
 // Make.com webhook configuration
 // Update these URLs with your actual Make.com webhook endpoints
 
-export const WEBHOOK_URL = "https://hook.eu2.make.com/4xfjeqve2vggj272duh7n2ht1k98e1mw";
-export const STATUS_URL = "https://hook.eu2.make.com/c2whxklnfgfc6435ddqty6pl0jyw2484";
+export const WEBHOOK_URL = "https://hook.eu2.make.com/xi4tj1g4fg9d2u68m1co6evr7kg32qdr";
+export const STATUS_URL = "https://hook.eu2.make.com/sia8lukw190bbdqc0ao122ga28ova5vq";
 
-export const MAKE_CREATE_URL = "https://hook.eu2.make.com/4xfjeqve2vggj272duh7n2ht1k98e1mw";
-export const MAKE_STATUS_URL = "https://hook.eu2.make.com/c2whxklnfgfc6435ddqty6pl0jyw2484";
+export const MAKE_CREATE_URL = "https://hook.eu2.make.com/xi4tj1g4fg9d2u68m1co6evr7kg32qdr";
+export const MAKE_STATUS_URL = "https://hook.eu2.make.com/sia8lukw190bbdqc0ao122ga28ova5vq";
 
 export const MAKE_API_KEY = "bager155";
 // src/config/make.ts
